2d7938a33e28f88fafe2646c760b411556d96586
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2023-09-24 17:29:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-5xl text-red-700">Welcome to the mega project </p>
+      <p className="text-green-600">Hello world</p>
+    </>
+  );
+}
+
+export default App;
